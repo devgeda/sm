974db078ee2b05890a6a3affdf8a5cc6e61fdfb6
Gabriel Menezes <65097161+devgeda@users.noreply.github.com>
2025-07-26 22:46:09 -0300

    chore(sm): atualizadas as configurações de migrations, docker, profiles. (#1)
    Atualizado as configurações para migrations adicionando o flyway no projeto, corrigido a estrutura de container a serem criados com docker e organizados as configurações de profile para o spring-boot.

--- estoque/fx/src/main/java/com/devgeda/fx/EstoqueJavaFxApplicationLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.devgeda.fx;
-
-import javafx.application.Application;
-
-public class EstoqueJavaFxApplicationLauncher {
-	public static void main(String[] args) {
-		Application.launch(EstoqueJavaFxApplication.class, args);
-	}
-}
